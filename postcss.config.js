c5e8b2b96408bd11d9b8fc718ab0c5f92db406b7
atingaev <atingaev@moysklad.ru>
2026-06-03 01:21:53 +0300

    Задание 1 * всё было старое, переехал на tailwind + flowbite 4

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,5 @@
-const tailwindcss       = require('tailwindcss');
-const postcssPresetEnv  = require('postcss-preset-env');
-
 module.exports = {
-    plugins: [
-        tailwindcss,
-        postcssPresetEnv({
-            stage: 1,
-            features: {
-                'is-pseudo-class': false,
-            },
-        }),
-    ],
+    plugins: {
+        '@tailwindcss/postcss': {},
+    },
 };
